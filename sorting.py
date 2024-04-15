50751e2508556b90cd59970697e04463307766b4
Viktor Morovic <257026@vutbr.cz>
2024-04-15 12:59:35 +0200

    bubble sort

--- sorting.py
+++ sorting.py
@@ -29,15 +29,15 @@ def read_data(file_name):
         return num_dict
 
 
-def selection_sort(numdict: dict, direction: bool = True):
+def selection_sort(num_dict: dict, direction: bool = True):
     """
     sorts values in a dictionary numdict
     :param direction: direction of the sort reverse == False
-    :param numdict: dictionary of numbers
+    :param num_dict: dictionary of numbers
     :return: numdict: sorted dictionary of numbers
     """
 
-    for value in numdict.values():
+    for value in num_dict.values():
         for i in range(0, len(value)):
             for num in value:
                 if direction:
@@ -51,12 +51,43 @@ def selection_sort(numdict: dict, direction: bool = True):
                     else:
                         continue
 
-    return numdict
+    return num_dict
+
+
+def bubble_sort(num_dict: dict):
+    """
+    sorts list within a dictionary using bubble sort method
+    :param num_dict: (dict): unsorted dictionary
+    :return: num_dict: sorted dictionary
+    """
+
+    for value in num_dict.values():
+        n = len(value)
+        for i in range(n - 1):
+            for num_idx in range(n - 1 - i):
+                if value[num_idx] > value[num_idx + 1]:
+                    value[num_idx], value[num_idx + 1] = value[num_idx + 1], value[num_idx]
+                else:
+                    continue
+
+    """
+    legacy code
+    
+    for value in num_dict.values():
+        for idx, num in enumerate(value):
+            while num > value[]:
+                if value.index(num) == (len(value) - 1 - idx):
+                    break
+                else:
+                    num, value[value.index(num) + 1] = value[value.index(num) + 1], num
+    """
+    return num_dict
 
 
 def main():
     # print(os.getcwd())
-    print(selection_sort(read_data("numbers.csv")))
+    # print(selection_sort(read_data("numbers.csv")))
+    print(bubble_sort(num_dict=read_data("numbers.csv")))
     # pass
 
 
