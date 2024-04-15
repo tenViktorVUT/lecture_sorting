fbcf04d9a12133b38fda260873f6abf652d5383e
Viktor Morovic <257026@vutbr.cz>
2024-04-15 11:55:48 +0200

    spracovanie dat

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 
@@ -8,11 +9,35 @@ def read_data(file_name):
     :param file_name: (str), name of CSV file
     :return: (dict), dictionary with numeric data, keys - csv column names, values - numbers in each column
     """
+
+    num_dict = dict()
+
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
 
+    with open(file_path, mode="r") as file:
+        reader = csv.DictReader(file)
+        iteration = 0
+        # print(reader)
+        for row in reader:
+            for key, value in row.items():
+                if iteration == 0:
+                    num_dict[key] = [int(value)]
+                else:
+                    num_dict[key].append(int(value))
+            iteration += 1
+        return num_dict
+
+def selection_sort(numdict: dict):
+    """
+    sorts values in a dictionary numdict
+    :param numdict:
+    :return:
+    """
 
 def main():
+    # print(os.getcwd())
+    print(read_data("numbers.csv"))
     pass
 
 
